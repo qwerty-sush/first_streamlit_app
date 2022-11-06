bb897069aefed0d6e28e404e1dac0d036d3b26fe
qwerty-sush <117585076+qwerty-sush@users.noreply.github.com>
2022-11-06 21:09:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.header('LIFE')
+streamlit.title('Weed of life')
 streamlit.text('YOU ONLY LIVE ONCE')
 streamlit.text('lets celebrate LIFE')
 streamlit.text('yaaaaa ----- hoooooooo******')
