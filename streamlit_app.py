3e258043bde9a9896981e75eefb1dc5fa7d1d17b
qwerty-sush <117585076+qwerty-sush@users.noreply.github.com>
2022-11-06 21:14:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.title('Weed of life')
 streamlit.text('YOU ONLY LIVE ONCE')
 streamlit.text('lets celebrate LIFE')
 streamlit.text('yaaaaa ----- hoooooooo******')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
